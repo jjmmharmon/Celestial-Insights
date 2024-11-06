aa9ad369ff6058e11452e6f7cacad77be4278083
Jon <jjmmharmon@gmail.com>
2024-11-06 16:33:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-// config.js
-const config = {
-    BASE_URL: window.location.hostname === 'localhost' 
-        ? 'http://localhost:5000' 
-        : 'https://your-app.herokuapp.com'
+// You can add any necessary configurations here
+module.exports = {
+    // MongoDB URI (should be placed in .env or app.env)
+    mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/mern-course",
+    
+    // Optional: Define other configurations like port, secret keys, etc.
+    secretOrKey: process.env.SECRET_KEY || "mysecretkey"
 };
-
-export default config;
