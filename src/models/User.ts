400d2a0fd2b52d6266d11c036bbab8d686750407
Jon <jjmmharmon@gmail.com>
2024-11-07 09:32:35 -0500

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,18 @@
-// src/models/User.ts
 import mongoose, { Schema, Document } from 'mongoose';
 
+// Define the interface for the User document
 interface IUser extends Document {
   username: string;
   email: string;
   password: string;
 }
 
+// Define the schema for the User model
 const UserSchema: Schema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
+// Export the User model
 export default mongoose.model<IUser>('User', UserSchema);
