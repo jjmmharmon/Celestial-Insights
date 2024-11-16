61a0f11e2c08169b7e6003e4c0d13ceb58267276
Jon <jjmmharmon@gmail.com>
2024-11-16 12:00:40 -0500

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGO_URI || 'your_mongo_connection_string';
-    
-    // Connect to MongoDB using Mongoose
+    const dbURI = process.env.MONGO_URI || 'your_mongo_connection_string'; // Fallback if MONGO_URI isn't set
     await mongoose.connect(dbURI);
-    
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
