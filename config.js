b54cee14547ad53fb6ac5f4705ed900045e57389
Jon <jjmmharmon@gmail.com>
2024-11-16 09:59:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-// You can add any necessary configurations here
-module.exports = {
-    // MongoDB URI (should be placed in .env or app.env)
-    mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/mern-course",
-    
-    // Optional: Define other configurations like port, secret keys, etc.
-    secretOrKey: process.env.SECRET_KEY || "mysecretkey"
+// config.js
+const config = {
+    BASE_URL: "http://localhost:5000/auth", // Backend base URL
 };
+
+export default config;
